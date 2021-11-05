264a9563bc9d6e7d9f90f0c5e0269c14908330b0
Marie Carmela Ramirez <mariecarmelar@gmail.com>
2021-11-05 14:05:53 +0800

    Assignment number 3

--- INFORMATION.py
+++
@@ -1,5 +0,0 @@
-name = input("Your Name:")
-age = input("Your age:")
-address = input("Your address:")
-
-print("Hi, my name is {name}. I am {age} years old and I live in {address}.")
--- money i have to buy apples.py
+++
@@ -1,7 +0,0 @@
-cash = int(input("Amount of money you have?: "))
-apple_price = int(input("How much is an apple?: "))
-
-capacity_to_buy = cash//apple_price
-money_change = cash%apple_price
-
-print("You can buy {capacity_to_buy} apples and your change is {money_change} pesos. ")
